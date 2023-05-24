7d8b791f0376d76298fdf30792b362520a8e5954
sirln <siro@sirlawren.com>
2023-05-24 23:07:43 +0000

    back with my simple ash shell

---
+++ ash.h
@@ -0,0 +1,62 @@
+#ifndef ASH_SHELL
+#define ASH_SHELL
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+/**
+  * struct memory - structure name
+  * @size: First member
+  * @data: Second member
+  *
+  *Description: memory block structure
+  *
+  */
+
+typedef struct memory
+{
+	size_t size;
+	char *data;
+} memory_block;
+
+
+extern char **environ;
+
+
+int main(int argc, char **argv);
+
+
+void signal_handler(int sig_num);
+void ash(char **command);
+void prompt(void);
+char *get_command(void);
+void parse_command(char *command, char **args);
+int run_command(char **command);
+
+
+char *generate_command_path(const char *path, const char *command);
+char *find_command_path(char *command);
+
+
+
+int _putchar(char c);
+void update_lineptr(char **lineptr, size_t *n, ssize_t total_chars);
+ssize_t ash_getline(char **lineptr, size_t *n, FILE *stream);
+void *ash_memcpy(char *dest, char *src, size_t size);
+void *ash_realloc(void *ptr, size_t size);
+
+
+int _strlen(const char *s);
+int _strcmp(const char *s1, const char *s2);
+char *_strdup(const char *str);
+char *_strcpy(char *dest, const char *src);
+char *_strcat(char *dest, const char *src);
+int _strncmp(const char *str1, const char *str2, size_t n);
+
+#endif
--- main.c
+++ main.c
@@ -1,66 +1,20 @@
-#include "main.h"
+#include "ash.h"
 
 /**
-  *display_prompt - prompt
+  *main - start of program execution
+  *
+  *@argc: number of arguments
+  *@argv: array of arguments
+  *
+  *Return: 0 on success and 1 on failure
+  *
   */
 
-void display_prompt(void)
+int main(int argc, char **argv)
 {
-	write(1, "$ ", 2);
-}
-
-
-/**
-  *execute_command - prompt
-  *@command: command
-  */
-void execute_command(char *command)
-{
-	pid_t pid = fork();
-	int status;
-
-	if (pid < 0)
-	{
-		perror("fork failed");
-		exit(EXIT_FAILURE);
-	}
-	else if (pid == 0)
-	{
-	if (execlp(command, command, NULL) == -1)
-	{
-		perror("execlp failed");
-		exit(EXIT_FAILURE);
-	}
-	}
-	else
-	{
-		waitpid(pid, &status, 0);
-	}
-}
-
-
-/**
-  *main - start
-  *Return: 0 on success
-  */
-int main(void)
-{
-	char command[MAX_COMMAND_LENGTH];
-
-	while (1)
-	{
-		display_prompt();
-
-		if (fgets(command, sizeof(command), stdin) == NULL)
-		{
-			write(1, "\n", 1);
-			break;
-		}
-
-		command[strcspn(command, "\n")] = '\0';
-
-		execute_command(command);
-	}
+	if (argc == 0)
+		return (-1);
+	ash(argv);
 
 	return (0);
 }
